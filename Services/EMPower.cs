b1247237dbdcfcca9a30db4f0921a5e52f24ec98
PNguyenm <ngmphuong@gmail.com>
2016-08-30 13:41:59 +0700

    Add files via upload

---
+++ Services/EMPower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OnlineProducts
+{
+    public class EMPower
+    {
+        private string main_node = "//Database[@name='" + INIT.EMPOWER + "']";
+        private string sql_GetClaim;
+
+        public List<clsClaim> Claims;
+        public EMPower()
+        {
+            sql_GetClaim = CommonFuncs.getNodeValue<string>(INIT.SQL_path, main_node + @"/Script[@name='Get_Claims']");
+        }
+        public List<clsClaim> GetClaims()
+        {
+            //return null;
+            //XmlReader xmlr = 
+            XmlDocument xml = DBConnection.selectData2XML(CONST_DATABASE_CONFIG.db_Empower.ConnString, sql_GetClaim);
+            //xml.Load(xmlr);
+            XmlNodeList nodelist = xml.GetElementsByTagName("claim");
+            List<clsClaim> lst = new List<clsClaim>();
+            foreach (XmlNode node in nodelist)
+            {
+                clsClaim claim = new clsClaim(node);
+                lst.Add(claim);
+            }
+            return lst;
+          // return CommonFuncs.getObjectsListFromDB<clsClaim>(CONST_DATABASE_CONFIG.db_Empower.ConnString, sql_GetClaim);
+        }
+    }
+}
---
+++ Services/Pivotal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnlineProducts
+{
+    public class Pivotal
+    {
+        private string main_node = "//Database[@name='" + INIT.PIVOTAL + "']";
+        private string sql_GetClaim; 
+
+        public List<clsClaim> Claims;
+        public Pivotal()
+        {
+            sql_GetClaim = CommonFuncs.getNodeValue<string>(INIT.SQL_path, main_node + @"/Script[@name='Get_Claims']");
+        }
+
+        public List<clsClaim> GetClaims()
+        {
+            return CommonFuncs.getObjectsListFromDB<clsClaim>(CONST_DATABASE_CONFIG.db_Pivotal.ConnString, sql_GetClaim);
+            //return null;
+        }
+    }
+}
